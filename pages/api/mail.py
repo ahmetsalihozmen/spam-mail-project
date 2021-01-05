1f27b055605a175395f8f1898943054eb6f8744d
ahmet <ahmetsalihozm@outlook.com>
2021-01-05 22:48:11 +0300

    area size problem fix

--- pages/api/mail.py
+++ pages/api/mail.py
@@ -21,10 +21,10 @@ from sklearn.neighbors import NearestNeighbors
 from sklearn.metrics import f1_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import precision_score
-import keras
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.utils import to_categorical 
+# import keras
+# from keras.models import Sequential
+# from keras.layers import Dense
+# from keras.utils import to_categorical 
 import os
 import json
 import sys
@@ -558,93 +558,93 @@ if(datasets_model[sys.argv[2]]==4):
 
 
 
-if(datasets_model[sys.argv[2]]==5):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
+# if(datasets_model[sys.argv[2]]==5):
+#     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
     
     
     
-    model = Sequential()
-    model.add(Dense(500, activation='relu', input_dim=1000))
-    model.add(Dense(100, activation='relu'))
-    model.add(Dense(50, activation='relu'))
-    model.add(Dense(1, activation='sigmoid'))
-    model.compile(optimizer='adam', 
-              loss='binary_crossentropy', 
-              metrics=['accuracy'])
-    history =model.fit(X_train, y_train, epochs=3,validation_data=(X_test,y_test),verbose=0)
-    # Predicting the Test set results
-    y_pred = model.predict(X_test)
-    scores = model.evaluate(X_train, y_train, verbose=0)
-    y_pred=np.around(y_pred)
+#     model = Sequential()
+#     model.add(Dense(500, activation='relu', input_dim=1000))
+#     model.add(Dense(100, activation='relu'))
+#     model.add(Dense(50, activation='relu'))
+#     model.add(Dense(1, activation='sigmoid'))
+#     model.compile(optimizer='adam', 
+#               loss='binary_crossentropy', 
+#               metrics=['accuracy'])
+#     history =model.fit(X_train, y_train, epochs=3,validation_data=(X_test,y_test),verbose=0)
+#     # Predicting the Test set results
+#     y_pred = model.predict(X_test)
+#     scores = model.evaluate(X_train, y_train, verbose=0)
+#     y_pred=np.around(y_pred)
       
-    tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
-    # accuracies = cross_val_score(estimator = model, X = X_train, y = y_train, cv = 10,scoring="accuracy")
-    x={"TN":int(tn),"FP":int(fp),"FN":int(fn),"TP":int(tp)}
+#     tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
+#     # accuracies = cross_val_score(estimator = model, X = X_train, y = y_train, cv = 10,scoring="accuracy")
+#     x={"TN":int(tn),"FP":int(fp),"FN":int(fn),"TP":int(tp)}
 
-    x['Acc']=float(accuracy_score(y_test, y_pred))
-    x['Mean']=""
-    x['STDV']=""
-    a=f1_score(y_test,y_pred,labels=[0,1],average=None)
-    a1=a[0]
-    a2=a[1]
-    b=recall_score(y_test,y_pred,labels=[0,1],average=None)
-    b1=b[0]
-    b2=b[1]
-    c=precision_score(y_test,y_pred,labels=[0,1],average=None)
-    c1=c[0]
-    c2=c[1]
-    x['RCS0']=b1
-    x['RCS1']=b2
-    x['PCS0']=c1
-    x['PCS0']=c2
-    x['F1S0']=a1
-    x['F1S1']=a2
+#     x['Acc']=float(accuracy_score(y_test, y_pred))
+#     x['Mean']=""
+#     x['STDV']=""
+#     a=f1_score(y_test,y_pred,labels=[0,1],average=None)
+#     a1=a[0]
+#     a2=a[1]
+#     b=recall_score(y_test,y_pred,labels=[0,1],average=None)
+#     b1=b[0]
+#     b2=b[1]
+#     c=precision_score(y_test,y_pred,labels=[0,1],average=None)
+#     c1=c[0]
+#     c2=c[1]
+#     x['RCS0']=b1
+#     x['RCS1']=b2
+#     x['PCS0']=c1
+#     x['PCS0']=c2
+#     x['F1S0']=a1
+#     x['F1S1']=a2
    
      
    
  
-    if(sys.argv[3]==""):
-         x['res_data']=""
-    else:
-        test=X_train[int(sys.argv[3])].reshape(1,-1)
-        sonuc=model.predict(test)
-        sonuc=int(np.around(sonuc))
-        if(sonuc==1):
+#     if(sys.argv[3]==""):
+#          x['res_data']=""
+#     else:
+#         test=X_train[int(sys.argv[3])].reshape(1,-1)
+#         sonuc=model.predict(test)
+#         sonuc=int(np.around(sonuc))
+#         if(sonuc==1):
            
-             x['res_data']="Spam"
+#              x['res_data']="Spam"
               
-        else:
+#         else:
             
-             x['res_data']="Spam"
+#              x['res_data']="Spam"
  
     
-    if(sys.argv[4]==""):
-        x['res_string']=""
-    else:
-        text=sys.argv[4]
-        sentence_vectors_test = []
+#     if(sys.argv[4]==""):
+#         x['res_string']=""
+#     else:
+#         text=sys.argv[4]
+#         sentence_vectors_test = []
            
-        sentence_tokens_test = nltk.word_tokenize(text)
-        sent_vec_test = []
-        for token in most_frequency:
+#         sentence_tokens_test = nltk.word_tokenize(text)
+#         sent_vec_test = []
+#         for token in most_frequency:
             
-            if token in sentence_tokens_test:
-                sent_vec_test.append(1)
-            else:
-                sent_vec_test.append(0)
-        sentence_vectors_test.append(sent_vec_test)
+#             if token in sentence_tokens_test:
+#                 sent_vec_test.append(1)
+#             else:
+#                 sent_vec_test.append(0)
+#         sentence_vectors_test.append(sent_vec_test)
                 
-        sentence_vectors_test = np.asarray(sentence_vectors_test)
-        test=sentence_vectors_test[0].reshape(1,-1)
-        sonuc=model.predict(test)
-        sonuc=int(np.around(sonuc))
+#         sentence_vectors_test = np.asarray(sentence_vectors_test)
+#         test=sentence_vectors_test[0].reshape(1,-1)
+#         sonuc=model.predict(test)
+#         sonuc=int(np.around(sonuc))
         
-        if(sonuc==1):
-            x['res_string']="Spam"
-        else:
-            x['res_string']="Spam degil"
-    y=json.dumps(x)
-    print(y)
+#         if(sonuc==1):
+#             x['res_string']="Spam"
+#         else:
+#             x['res_string']="Spam degil"
+#     y=json.dumps(x)
+#     print(y)
 
     
     
